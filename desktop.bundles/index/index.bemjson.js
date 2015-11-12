0900d9bb040894b1219253fd0fa315fc05218c87
Mikhail <mikhail@koloskov.kz>
2015-11-13 00:33:29 +0300

    document news position top-news user

--- desktop.bundles/index/index.bemjson.js
+++ desktop.bundles/index/index.bemjson.js
@@ -1,33 +1,144 @@
 module.exports = {
-    block : 'page',
-    title : 'Title of the page',
-    favicon : '/favicon.ico',
-    head : [
-        { elem : 'meta', attrs : { name : 'description', content : '' } },
-        { elem : 'meta', attrs : { name : 'viewport', content : 'width=device-width, initial-scale=1' } },
-        { elem : 'css', url : 'index.min.css' }
-    ],
-    scripts: [{ elem : 'js', url : 'index.min.js' }],
-    mods : { theme : 'islands' },
-    content : [
+  block : 'page',
+  title : 'bem-kit',
+  favicon : '/favicon.ico',
+  head : [
+    { elem : 'meta', attrs : { name : 'description', content : '' } },
+    { elem : 'meta', attrs : { name : 'viewport', content : 'width=device-width, initial-scale=1' } },
+    { elem : 'css', url : 'index.min.css' }
+  ],
+  scripts: [{ elem : 'js', url : 'index.min.js' }],
+  mods : { theme : 'islands' },
+  content : [
+    {
+      block : 'row',
+      content : [
         {
-            block : 'row',
-            content : [
+          elem : 'col',
+          mods : { sw : 12, mw : 6, lw : 6, xl : 6, xxl : 4 },
+          content : [
+
+            {
+              block: 'position',
+              content: [
                 {
-                    elem : 'col',
-                    mods : { sw : 2, mw : 4, lw : 12, xl : 12, xxl : 12 },
-                    content : [
-                        'Первая колонка'
-                    ]
+                    elem : 'image'
                 },
                 {
-                    elem : 'col',
-                    mods : { sw : 10, mw : 8, lw : 0, xl : 0 },
-                    content : [
-                        'Вторая колонка'
+                    elem : 'text',
+                    content : 'Front-end developer'
+                },
+              ]
+            },
+
+
+            {
+              block: 'document',
+              content: [
+                {
+                  elem : 'image',
+                  content : ''
+                },
+                {
+                  elem : 'content',
+                  content: [
+                    {
+                      block: 'document-content',
+                      content: [
+                        {
+                          elem : 'title',
+                          content : 'Pellentesque habitant'
+                        },
+                        {
+                          elem : 'text',
+                          content : 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.'
+                        },
+                      ]
+                    }
+                  ]
+                },
+              ]
+            },
+            {
+              block: 'user',
+              content: [
+                {
+                    elem : 'image',
+                    content : ''
+                },
+                {
+                    elem : 'content',
+                    content: [
+                      {
+                        block: 'user-content',
+                        content: [
+                          {
+                              elem : 'name',
+                              content : 'Mikhail Koloskov'
+                          },
+                          {
+                              elem : 'position',
+                              content : 'UI Engineer'
+                          },
+                        ]
+                      }
                     ]
-                }
-            ]
+                },
+              ]
+            },
+            {
+              block: 'top-news',
+              content: [
+                {
+                    elem : 'date',
+                    content : '16.03.2015'
+                },
+                {
+                    elem : 'text',
+                    content : 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.'
+                },
+              ]
+            },
+            {
+              block: 'news',
+              content: [
+                {
+                    elem : 'image'
+                },
+                {
+                    elem : 'content',
+                    content: [
+                      {
+                          block : 'news-content',
+                          content: [
+                            {
+                                elem : 'title',
+                                content : 'Название новости'
+                            },
+                            {
+                                elem : 'date',
+                                content : '16.03.2015'
+                            },
+                            {
+                                elem : 'text',
+                                content : 'текст новости'
+                            },
+                          ]
+                      },
+                    ]
+                },
+              ]
+            }
+          ]
+        },
+        {
+          elem : 'col',
+          mods : { sw : 10, mw : 8, lw : 0, xl : 0 },
+          content : [
+            'Вторая колонка'
+          ]
         }
-    ]
+      ]
+    }
+  ]
 };
